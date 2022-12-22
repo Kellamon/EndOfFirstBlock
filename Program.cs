2a67c40fb2b2739de389e5a9ff3bb1c06020ac9f
Kellamon <fqvxiwzz5iym@mail.ru>
2022-12-22 18:28:08 +0300

    Добавлено описание задачи в README файл

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] NewArray = new string[3] { "Hello", "222", "World" };
+string[] CompareArray = new string[NewArray.Length];
+
+void Compare(string[] NewArray, string[] CompareArray)
+{
+    int count = 0;
+    for (int i = 0; i < NewArray.Length; i++)
+    {
+        if (NewArray[i].Length <= 3)
+        {
+            CompareArray[count] = NewArray[i];
+            count++;
+        }
+        else
+            Console.WriteLine($"{i + 1}й не соответсвует ТЗ");
+    }
+}
